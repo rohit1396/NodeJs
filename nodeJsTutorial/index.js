87e0dee1bcb5aa5ffe44b7650796a19cd3036e33
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-11-28 19:42:43 +0530

    first commit

---
+++ nodeJsTutorial/index.js
@@ -0,0 +1,20 @@
+// console.log("welcome to nodeJs Tutorial");
+
+const fs = require("fs");
+// fs.mkdirSync("nodeTutorial");
+
+// fs.writeFileSync(
+//   "nodeTutorial/bio.txt",
+//   "Hello, welcome to programming world "
+// );
+
+// fs.appendFileSync("nodeTutorial/bio.txt", " Let's dive into NodeJs");
+
+// const data = fs.readFileSync("nodeTutorial/bio.txt", "utf8");
+// console.log(data);
+
+// fs.renameSync("nodeTutorial/bio.txt", "nodeTutorial/myBio.txt");
+
+// fs.unlinkSync("nodeTutorial/myBio.txt");
+
+// fs.rmdirSync("nodeTutorial");
